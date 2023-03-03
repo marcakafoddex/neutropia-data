d5fba4a05b0ed5bc99fcd41d9b664134a4971c6d
Marc Oude Kotte <foddex@foddex.net>
2023-03-03 15:08:13 +0100

    Made font shader and renamed outline shader

---
+++ shaders/mesh-font.fs
@@ -0,0 +1,39 @@
+layout (location = 0) in vec2 vUv;
+layout (location = 1) in vec4 vColor;
+
+layout (location = 0) out vec4 FragColor;
+
+uniform sampler2D gTexture;
+uniform vec3 gBorderColor = vec3(0, 0, 0);
+uniform vec2 gPixelScale = vec2(2, 2);
+
+const float kAlphaThreshold = 0.5;
+
+void main()
+{
+	vec4 color = texture(gTexture, vUv) * vColor;
+	if (color.a > kAlphaThreshold)
+		FragColor = color;
+	else {
+		vec2 texSize = textureSize(gTexture, 0);
+		vec2 nextPixelUvOffset = gPixelScale / texSize;
+		if (false ||
+			// 3 pixels to our right
+			(texture(gTexture, vec2(vUv.x + nextPixelUvOffset.x, 	vUv.y - nextPixelUvOffset.y	)) * vColor).a > kAlphaThreshold ||
+			(texture(gTexture, vec2(vUv.x + nextPixelUvOffset.x, 	vUv.y						)) * vColor).a > kAlphaThreshold ||
+			(texture(gTexture, vec2(vUv.x + nextPixelUvOffset.x, 	vUv.y + nextPixelUvOffset.y	)) * vColor).a > kAlphaThreshold ||
+			// 3 pixels to our left
+			(texture(gTexture, vec2(vUv.x - nextPixelUvOffset.x, 	vUv.y - nextPixelUvOffset.y	)) * vColor).a > kAlphaThreshold ||
+			(texture(gTexture, vec2(vUv.x - nextPixelUvOffset.x, 	vUv.y						)) * vColor).a > kAlphaThreshold ||
+			(texture(gTexture, vec2(vUv.x - nextPixelUvOffset.x, 	vUv.y + nextPixelUvOffset.y	)) * vColor).a > kAlphaThreshold ||
+			// 2 pixels above/below
+			(texture(gTexture, vec2(vUv.x, 							vUv.y + nextPixelUvOffset.y	)) * vColor).a > kAlphaThreshold ||
+			(texture(gTexture, vec2(vUv.x, 							vUv.y - nextPixelUvOffset.y	)) * vColor).a > kAlphaThreshold ||
+			
+			false)
+			FragColor = vec4(gBorderColor, 1);
+		else
+			FragColor = vec4(0, 0, 0, 0);
+	}
+	
+}
