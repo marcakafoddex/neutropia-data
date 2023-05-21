af83912834757e710c2230d08bc0ddf34853557f
Marc Oude Kotte <foddex@foddex.net>
2023-05-21 23:27:28 +0200

    Shader update

--- shaders/mesh-map-line.fs
+++ shaders/mesh-map-line.fs
@@ -1,10 +1,9 @@
 layout (location = 0) flat in vec3 startPos;
 layout (location = 1) in vec3 vertPos;
-layout (location = 2) in vec4 vColor;
 
 layout (location = 0) out vec4 FragColor;
 
-// uniform vec4 gOutlineColor;
+uniform vec4 gLineColor;
 
 uniform vec2  gResolution;
 uniform float gDashSize;
@@ -18,5 +17,5 @@ void main()
     if (fract(dist / (gDashSize + gGapSize)) > gDashSize/(gDashSize + gGapSize))
         discard;
 
-    FragColor = vColor;
+    FragColor = gLineColor;
 }
